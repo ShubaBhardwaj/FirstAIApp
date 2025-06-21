78f4f67a5159b354617c5b8a94badf87d1c0783c
Shubham Bhardwaj <bhardwajshub0001@gmail.com>
2025-06-21 14:50:44 +0530

    setup the all routes of the tickets and the inngest

--- ai-ticket-assistant/index.js
+++ ai-ticket-assistant/index.js
@@ -3,6 +3,12 @@ import mongoose from "mongoose"
 import cors from "cors"
 import dotenv from "dotenv"
 import userRoutes from "./routers/user.js"
+import ticketRoutes from "./routers/ticket.js"
+import { serve } from "inngest/express"
+import { inngest } from "./inngest/client.js"
+import { onSignup } from "./inngest/functions/onSignup.js"
+import { onticketCreated } from "./inngest/functions/onTicketCreation.js"
+
 dotenv.config()
 
 
@@ -12,7 +18,16 @@ const app = express()
 app.use(cors()) // Its like a middleware which helps in the connecting with the frontend 
 app.use(express.json()) // This for accepting the json data
 
-app.use("/api/auth", userRoutes)
+app.use("/api/auth", userRoutes);
+app.use("/api/tickets", ticketRoutes);
+
+app.use(
+    "api/innges",
+    serve({
+        client: inngest,
+        functions: [onSignup, onticketCreated]
+    })
+);
 
 mongoose
        .connect(mongoose_url)
--- ai-ticket-assistant/routers/user.js
+++ ai-ticket-assistant/routers/user.js
@@ -1,5 +1,16 @@
 import express from "express"
+import { getUser, login, logOut, signup, updateUser } from "../controllers/user";
+import{authenticate} from "../middlewares/auth.js"
 
 const router = express.Router()
+
+
+router.post("/update-user", authenticate, updateUser);
+router.get("/users", authenticate, getUser);
+
+
+router.post("/signup",signup);
+router.post("/login",login);
+router.post("/logout",logOut);
 
 export default router;
